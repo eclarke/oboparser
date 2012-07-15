b69102bca15d6420b13d1bcfc1a9083890899e59
Erik Clarke <erikclarke@gmail.com>
2012-07-15 11:46:22 -0700

    more general, added main() access point for CLI

--- obo.py
+++ obo.py
@@ -1,47 +1,51 @@
 import sqlite3
 from collections import defaultdict
+from optparse import OptionParser
+
 
 TERM_SCHEMA_SQL = """CREATE TABLE terms(id, name, def, comment, synonym,
     unique(id) on conflict replace)"""
 
 
-HEIR_SCHEMA_SQL = """CREATE TABLE heirarchy(id, rel, related,
-    unique(id, rel, related) on conflict replace
-    foreign key(id) references terms(id))"""
+GRAPH_SCHEMA_SQL = """CREATE TABLE graph(subject, predicate, object,
+    unique(subject, predicate, object) on conflict replace
+    foreign key(subject) references terms(id))"""
 
 
 INSERT_SQL = """INSERT INTO terms(id, name, def)
 VALUES(?, ?, ?)"""
 
 
-INSERT_HEIR_SQL = """INSERT INTO heirarchy(id, rel, related)
+INSERT_GRAPH_SQL = """INSERT INTO graph(subject, predicate, object)
 VALUES(?, ?, ?)"""
 
 
-CHILDREN_SQL = """SELECT id FROM heirarchy WHERE rel like 'is_a'
-AND related like ?"""
+DESC_SQL = """SELECT subject FROM graph WHERE predicate like ?
+AND object like ?"""
 
 
-PARENT_SQL = """SELECT related FROM heirarchy WHERE rel like 'is_a'
-AND id like ?"""
+ASC_SQL = """SELECT object FROM graph WHERE predicate like ?
+AND subject like ?"""
 
 
-VALID_RELATIONSHIPS = ['is_a', 'relationship']
+MULTIVALUE = ['is_a', 'relationship']
 
 
 class Obo:
 
     conn = None
 
-    def __init__(self, obofile):
-        self.conn = self._createdb()
-        for term in self._parse_obo(obofile):
-            self._ins_stanza(term)
+    def __init__(self, obofile=None, dbname=':memory:', new=True):
+        self.conn = self._createdb(dbname, new)
+        if obofile:
+            for term in self._parse_obo(obofile):
+                self._ins_stanza(term)
 
-    def _createdb(self):
-        conn = sqlite3.connect(':memory:')
-        conn.execute(TERM_SCHEMA_SQL)
-        conn.execute(HEIR_SCHEMA_SQL)
+    def _createdb(self, dbname, new):
+        conn = sqlite3.connect(dbname)
+        if new:
+            conn.execute(TERM_SCHEMA_SQL)
+            conn.execute(GRAPH_SCHEMA_SQL)
         return conn
 
     def _parse(self, line):
@@ -79,7 +83,7 @@ class Obo:
             else:
                 for k, v in kvals.iteritems():
                     kvals[k] = v[0] if (len(v) == 1
-                        and k not in VALID_RELATIONSHIPS) else v
+                        and k not in MULTIVALUE) else v
                 stanza = kvals
                 kvals = defaultdict(list)
 
@@ -92,29 +96,42 @@ class Obo:
                         (stanza['id'], stanza['name'].decode('utf-8'), stanza['def'].decode('utf-8')))
         if 'is_a' in stanza:
             for related in stanza['is_a']:
-                self.conn.execute(INSERT_HEIR_SQL,
+                self.conn.execute(INSERT_GRAPH_SQL,
                     (stanza['id'], 'is_a', related))
         if 'relationship' in stanza:
             for related in stanza['relationship']:
                 relationship, target = self._parse_relationship(related)
-                self.conn.execute(INSERT_HEIR_SQL,
+                self.conn.execute(INSERT_GRAPH_SQL,
                     (stanza['id'], relationship, target))
 
-    def _find_related(self, relation, goid, infer):
+    def find_children(self, id, expand=False):
+        return self.query(id, 'is_a', expand=expand)
+
+    def find_parents(self, id, expand=False):
+        return self.query(id, 'is_a', subject=False, expand=expand)
+
+    def query(self, id, predicate, subject=True, expand=True):
         related = []
-        sql = CHILDREN_SQL if relation == 'child' else PARENT_SQL
-        c = self.conn.execute(sql, (goid,))
+        sql = DESC_SQL if subject else ASC_SQL
+        c = self.conn.execute(sql, (predicate, id))
         rset = c.fetchall()
         c.close()
         for relative in rset:
             related.append(relative[0])
-            if infer:
-                for sub in self._find_related(relation, relative[0], infer):
+            if expand:
+                for sub in self.query(relative[0], predicate, subject, expand):
                     related.append(sub)
         return related
 
-    def find_children(self, goid, infer=False):
-        return self._find_related("child", goid, infer)
+    def show_relations(self):
+        c = self.conn.execute("select distinct predicate from graph")
+        rset = [x[0] for x in c.fetchall()]
+        c.close()
+        return rset
 
-    def find_parents(self, goid, infer=False):
-        return self._find_related("parent", goid, infer)
+def main():
+    parser = OptionParser()
+    parser.add_option("")
+
+if __name__ == '__main__':
+    main()
