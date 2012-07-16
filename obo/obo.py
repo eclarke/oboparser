938d74083ca81468582844c358cca84b4fea6320
Erik Clarke <erikclarke@gmail.com>
2012-07-16 16:22:32 -0700

    package organization, use dict for term metadata

--- obo/obo.py
+++ obo/obo.py
@@ -6,32 +6,22 @@ over given predicate relationships.
 """
 import sqlite3
 from collections import defaultdict
+from orderedset import OrderedSet
 
 
-TERM_SCHEMA_SQL = """CREATE TABLE terms(id, name, def, comment, synonym,
-    unique(id) on conflict replace)"""
-
-
-GRAPH_SCHEMA_SQL = """CREATE TABLE graph(subject, predicate, object,
-    unique(subject, predicate, object) on conflict replace
-    foreign key(subject) references terms(id))"""
-
-
-INSERT_SQL = """INSERT INTO terms(id, name, def)
-VALUES(?, ?, ?)"""
-
+GRAPH_SCHEMA_SQL = """
+CREATE TABLE graph(subject, predicate, object,
+    unique(subject, predicate, object) on conflict replace);
+"""
 
 INSERT_GRAPH_SQL = """INSERT INTO graph(subject, predicate, object)
 VALUES(?, ?, ?)"""
 
-
 DESC_SQL = """SELECT subject FROM graph WHERE predicate like ?
 AND object like ?"""
 
-
 ASC_SQL = """SELECT object FROM graph WHERE predicate like ?
 AND subject like ?"""
-
 
 MULTIVALUE = ['is_a', 'relationship']
 
@@ -40,7 +30,7 @@ def _parse(line):
     """Returns a line split on a ': ' as a key/value."""
     if ':' in line:
         key, val = line.split(': ', 1)
-        val = val.strip('\n ')
+        val = val.strip('\n ').decode('utf-8')
         return key, val
     else:
         return line, None
@@ -56,7 +46,7 @@ def _strip_comments(string):
         return string
 
 
-def _parse_obo(obofile):
+def parse_obo(obofile):
     """Yields stanzas as encountered in OBO file as dicts."""
     stanza = None
     kvals = defaultdict(list)
@@ -66,11 +56,13 @@ def _parse_obo(obofile):
         key, val = _parse(line)
         if val:
             kvals[key].append(_strip_comments(val))
-        else:
+        elif 'id' in kvals:
             for key, val in kvals.iteritems():
                 kvals[key] = val[0] if (len(val) == 1
                     and key not in MULTIVALUE) else val
             stanza = kvals
+            kvals = defaultdict(list)
+        else:
             kvals = defaultdict(list)
 
 
@@ -88,33 +80,24 @@ class Obo:
     available relationships.
     """
     conn = None
+    stanzas = None
 
-    def __init__(self, obofile=None, dbname=':memory:', new=True):
-        """Instantiates a new Obo object, by default using an in-memory db.
-        If obofile given, imports into db. If dbname given, uses on-disk db."""
-        self._createdb(dbname, new)
+    def __init__(self, obofile):
+        """Creates a new ontology based off a given OBO file."""
+        self._createdb()
+        self.stanzas = {}
         if obofile:
-            for term in _parse_obo(obofile):
-                self._ins_stanza(term)
+            for stanza in parse_obo(obofile):
+                self.stanzas[stanza['id']] = stanza
+                self._ins_stanza(stanza)
 
-    def _createdb(self, dbname, new):
-        """Creates a connection to the specified SQLite db."""
+    def _createdb(self, dbname=':memory:'):
         self.conn = sqlite3.connect(dbname)
-        if new:
-            self.conn.execute(TERM_SCHEMA_SQL)
-            self.conn.execute(GRAPH_SCHEMA_SQL)
+        self.conn.execute(GRAPH_SCHEMA_SQL)
 
     def _ins_stanza(self, stanza):
-        """Inserts a stanza into the db, accounting for is_a and other
-        relationships."""
-        required_fields = set(('id', 'name', 'def'))
-        if required_fields.issubset(stanza.keys()):
-            if 'is_obsolete' in stanza and stanza['is_obsolete']:
+        if 'is_obsolete' in stanza and stanza['is_obsolete']:
                 return
-            self.conn.execute(INSERT_SQL,
-                        (stanza['id'],
-                         stanza['name'].decode('utf-8'),
-                         stanza['def'].decode('utf-8')))
         if 'is_a' in stanza:
             for related in stanza['is_a']:
                 self.conn.execute(INSERT_GRAPH_SQL,
@@ -124,6 +107,9 @@ class Obo:
                 relationship, target = _parse_relationship(related)
                 self.conn.execute(INSERT_GRAPH_SQL,
                     (stanza['id'], relationship, target))
+
+    def get(self, _id):
+        return self.stanzas.get(_id, None)
 
     def find_children(self, _id, expand=False):
         """Returns children (using 'is_a' relation) of given id.
@@ -135,24 +121,28 @@ class Obo:
         If expand=True, returns all [great,...] grandparents."""
         return self.query(_id, 'is_a', desc=False, expand=expand)
 
-    def query(self, _id, predicate, desc=True, expand=True):
+    def most_recent_common_ancestor(self, _id1, _id2, predicate):
+        p1 = self.query(_id1, predicate, desc=False, expand=True)
+        p2 = self.query(_id2, predicate, desc=False, expand=True)
+
+    def query(self, _id, predicate, desc=True, expand=False):
         """Returns all the descendants of a given predicate relationship, or
         if desc=False, returns ascendants. If expand=True, traverses up or
-        down tree to terminal nodes."""
-        related = []
+        down graph to terminal nodes."""
+        related = OrderedSet()
         sql = DESC_SQL if desc else ASC_SQL
         cur = self.conn.execute(sql, (predicate, _id))
         rset = cur.fetchall()
         cur.close()
         for relative in rset:
-            related.append(relative[0])
+            related.add(relative[0])
             if expand:
                 for sub in self.query(relative[0], predicate, desc, expand):
-                    related.append(sub)
+                    related.add(sub)
         return related
 
-    def show_relations(self):
-        """Shows all the predicate relationships from the graph."""
+    def relationships(self):
+        """Shows all the relationships in the graph."""
         cur = self.conn.execute("select distinct predicate from graph")
         rset = [x[0] for x in cur.fetchall()]
         cur.close()
